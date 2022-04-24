27058178a4de9d642add211aa29bbf0bfa0a38b2
NanRole <104119376+NanRole@users.noreply.github.com>
2022-04-24 15:05:07 +0800

    Update L.py

--- 110-2/0419/L.py
+++ 110-2/0419/L.py
@@ -1,17 +1,19 @@
 from microbit import *
 
+sleep_time = 1000
+
 while True:
   for i in range(4, -1, -1):
     for j in range(5-i):
       display.set_pixel(i, j, 9)
-    for j in range(i+1, 5):
+    for j in range(i, 5):
       display.set_pixel(j, 4-i, 9)
-    sleep(300)
+    sleep(sleep_time)
   for i in range(0, 5):
     for j in range(5-i):
       display.set_pixel(i, j, 0)
-    for j in range(i+1, 5):
+    for j in range(i, 5):
       display.set_pixel(j, 4-i, 0)
-    sleep(300)
+    sleep(sleep_time)
   display.clear()
-  sleep(300)
+  sleep(sleep_time)
