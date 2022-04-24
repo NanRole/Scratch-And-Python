36805ab8a1c2dd676dd8b25f2b8a8219d6954d0c
NanRole <104119376+NanRole@users.noreply.github.com>
2022-04-24 15:05:46 +0800

    Update L.py

--- 110-2/0419/L.py
+++ 110-2/0419/L.py
@@ -4,8 +4,10 @@ sleep_time = 1000
 
 while True:
   for i in range(4, -1, -1):
+    # |
     for j in range(5-i):
       display.set_pixel(i, j, 9)
+    # ——
     for j in range(i, 5):
       display.set_pixel(j, 4-i, 9)
     sleep(sleep_time)
